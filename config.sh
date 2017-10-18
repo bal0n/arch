559d1229a80d93faa259e2a5cbb11be3f2b62a51
Francisco de Javier <32569754+franciscodejavier@users.noreply.github.com>
2017-10-19 00:47:52 +0200

    Añado la instalación de Yaourt
    [AUR]

--- config.sh
+++ config.sh
@@ -8,4 +8,10 @@ systemctl enable NetworkManager
 useradd -m -g users -G audio,lp,optical,storage,video,wheel,games,power,scanner -s /bin/bash $nombreusuario
 passwd $nombreusuario
 nano /etc/sudoers
+
+echo -e "[!] Instalando Yaourt"
+echo -e "[archlinuxfr]\nSigLevel = Never\nServer = http://repo.archlinux.fr/\$arch" >> /etc/pacman.conf 
+sudo pacman -Sy
+sudo pacman -S yaourt
+
 exit
