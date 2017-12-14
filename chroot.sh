8c8fbafc63e4da2fe31efe77dc23187ad40de253
bal0n <franciscusxaverius@protonmail.com>
2017-12-14 21:10:25 +0100

    Simplificación de código básico

--- chroot.sh
+++ chroot.sh
@@ -4,21 +4,21 @@ nombre_de_equipo=vantpc
 nombreusuario=javier
 
 function config {
-	echo $nombre_de_equipo >> /etc/hostname  
-	rm /etc/localtime
-	ln -s /usr/share/zoneinfo/Europe/Madrid /etc/localtime  
-	echo "LANG=es_ES.UTF-8" >> /etc/locale.conf  
-  mv /etc/locale.gen /etc/locale.gen.bk
-	echo "es_ES.UTF-8 UTF-8" >> /etc/locale.gen 
-	locale-gen
-	echo "KEYMAP=es" >> /etc/vconsole.conf
-	#useradd -m -g users -G audio,lp,optical,storage,video,wheel,games,power,scanner -s /bin/bash $nombreusuario
-	#passwd $nombreusuario
+    echo $nombre_de_equipo >> /etc/hostname  
+    rm /etc/localtime
+    ln -s /usr/share/zoneinfo/Europe/Madrid /etc/localtime  
+    echo "LANG=es_ES.UTF-8" >> /etc/locale.conf  
+    mv /etc/locale.gen /etc/locale.gen.bk
+    echo "es_ES.UTF-8 UTF-8" >> /etc/locale.gen 
+    locale-gen
+    echo "KEYMAP=es" >> /etc/vconsole.conf
+    #useradd -m -g users -G audio,lp,optical,storage,video,wheel,games,power,scanner -s /bin/bash $nombreusuario
+    #passwd $nombreusuario
 }
 
 function grub {
-	grub-install /dev/sda
-	grub-mkconfig -o /boot/grub/grub.cfg
+    grub-install /dev/sda
+    grub-mkconfig -o /boot/grub/grub.cfg
 }
 
 config
--- script.sh
+++ script.sh
@@ -3,42 +3,45 @@
 
 # Funciones
 
-function discos {
-	mkfs.ext2 /dev/sda1
-	mkfs.ext4 /dev/sda2
-	mkswap /dev/sda3
-	swapon /dev/sda3
+function adminDiscos {
 
-	mount /dev/sda2 /mnt
-	mkdir /mnt/boot
-	mount /dev/sda1 /mnt/boot
+    # Prólogo
+    mkfs.ext2 /dev/sda1
+    mkfs.ext4 /dev/sda2
+    mkswap /dev/sda3
+    swapon /dev/sda3
+
+    # Nudo
+    mount /dev/sda2 /mnt
+    mkdir /mnt/boot
+    mount /dev/sda1 /mnt/boot
 }
 
-function base {
-	pacstrap /mnt base base-devel grub-bios networkmanager xf86-input-synaptics
-	genfstab -U -p /mnt >> /mnt/etc/fstab
+function instalacionBase {
+    pacstrap /mnt base base-devel grub-bios networkmanager xf86-input-synaptics
+    genfstab -U -p /mnt >> /mnt/etc/fstab
 }
 
-function chroot {
-	cp chroot.sh /mnt
-	chmod +x /mnt/chroot.sh
-	arch-chroot /mnt ./chroot.sh
-	umount /mnt/boot
-	umount /mnt
+function jaulaChroot {
+    cp chroot.sh /mnt
+    chmod +x /mnt/chroot.sh
+    arch-chroot /mnt ./chroot.sh
+    umount /mnt/boot
+    umount /mnt
 }
 
 # Main
 
 loadkeys es
-discos
+adminDiscos
 
 if ping -c1 google.com &> /dev/null; then
-	echo OK;
-	base;
+    echo OK;
+    instalacionBase;
 else
-	echo Conexion failed;
-	wifi-menu;
+    echo Conexion failed;
+    wifi-menu;
 fi
 
-chroot
+jaulaChroot
 reboot
