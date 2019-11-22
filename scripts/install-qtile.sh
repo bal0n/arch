82665e7c953c6d4b980ab51ac98b900cabf9d21a
fjbalon <fjba@pm.me>
2019-11-22 13:46:51 +0100

    Mudados scripts

--- scripts/install-qtile.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# More info: https://wiki.archlinux.org/index.php/Qtile
-remote=https://gitlab.com/theoslogos/qtile
-
-sudo pacman -S qtile
-git clone https://gitlab.com/theoslogos/qtile ~/.config/qtile
-#mkdir -p ~/.config/qtile/
-#cp /usr/share/doc/qtile/default_config.py ~/.config/qtile/config.py
