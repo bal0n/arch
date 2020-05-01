d5bc5e98710c65bd676dfae6da078ecdb72d954e
fjba <fjba@pm.me>
2020-05-01 20:52:47 +0200

    Añadido logs para testing

--- chroot.sh
+++ chroot.sh
@@ -9,6 +9,34 @@ pckgs=pckgs
 scriptsDir=scripts
 uefi=false
 
+RED='\033[0;31m'
+BLUE="\033[0;34m"
+GREEN="\033[0;32m"
+NC='\033[0m' # No Color
+verbose=true
+
+log() {
+    if [  $verbose = true  ]; then
+        type=$1
+        msg=$2
+        time=$(date '+%d/%m/%Y %H:%M:%S')
+        color=$BLUE
+
+        if [  $type = "ok"  ]; then
+            color=$GREEN
+        else 
+            if [  $type = "error"  ]; then
+                color=$RED
+            fi
+        fi
+
+        echo -e "$time ${color}[$type]${NC} $msg"
+        if [  $type = "error"  ]; then
+            exit 1
+        fi
+    fi
+}
+
 # Funciones
 function installPckgs {
 
@@ -21,34 +49,47 @@ function installPckgs {
     #    Reinstalación de paquetes de backup
     #    # pacman -S - < pkglist.txt
 
+    log info "Inicialización de instalación de paquetes"
     pacman -Sy --noconfirm $(<pckgs)
 }
 
 
 function scriptsRocket {
+    log info "Lanzamiento de scripts alojados en scripts/"
     sh $scripts/*.sh
 }
 
 function configGeneral {
+    log info "Actualización de hostname"
     echo $hostname >> /etc/hostname
+    log info "Inicialización de configuración de tiempo"
     rm /etc/localtime
     ln -s /usr/share/zoneinfo/Europe/Madrid /etc/localtime
+    log ok "localtime"
     echo "LANG=es_ES.UTF-8" >> /etc/locale.conf
+    log ok "locale.conf"
     mv /etc/locale.gen /etc/locale.gen.bk
     echo "es_ES.UTF-8 UTF-8" >> /etc/locale.gen
+    log ok "locale.gen"
     locale-gen
     echo "KEYMAP=es" >> /etc/vconsole.conf
+    log ok "vconsole.conf"
     # Instalación de directorios personales
     #xdg-user-dirs-update
     hwclock -w
+    log ok "hwclock"
     systemctl enable acpid.service
 }
 
 function configUsuario {
     # Configuración de usuario
+    log info "Iniciada configuración de usuario"
     useradd -m -g users -G audio,lp,optical,storage,video,wheel,games,power,scanner -s /bin/bash $username
+    log ok "Usuario creado"
     printf "$passu\n$passu" | passwd $username
+    log ok "Contraseña de usuario implementada"
     echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers
+    log ok "Usuario añadido al grupo sudoers mediante el grupo wheels"
 }
 
 function configRed {
@@ -68,34 +109,37 @@ function configAUR {
     makepkg -si
     sudo sed -i '/^#Color/s/^#//' /etc/pacman.conf
     aurman -Syu
+    log ok "aurman instalado"
 }
 
 
 # Configuración de Grub
 function configGrub {
+    # TODO: GRUB se instala a pelo en SDA, cambiar por variable
     if [  $uefi = true ]; then
+        log info "grub en modo UEFI"
         grub-install --efi-directory=/boot/efi --bootloader-id='Arch Linux' --target=x86_64-efi
     else
+        log info "grub en modo BIOS"
         grub-install /dev/sda
     fi
 
     grub-mkconfig -o /boot/grub/grub.cfg
-}
-
-# DEPRECATED
-function getFiles {
-    sh deployment.sh
+    log ok "GRUB instalado correctamente"
 }
 
 # Guión
 installPckgs
 configGeneral
 configUsuario
-configRed
+configRed# DEPRECATED
+function getFiles {
+    sh deployment.sh
+}
 configAUR
 configGrub
-#getFiles
 
 mkinitcpio -p linux
+log ok "mkinitcpio"
 printf "$passroot\n$passroot" | passwd
 exit
--- root.sh
+++ root.sh
@@ -9,6 +9,34 @@ home=/dev/nvme0n1p3 #/dev/sda3
 swap=/dev/nvme0n1p4 #/dev/sda4
 uefi=false
 
+RED='\033[0;31m'
+BLUE="\033[0;34m"
+GREEN="\033[0;32m"
+NC='\033[0m' # No Color
+verbose=true
+
+log() {
+    if [  $verbose = true  ]; then
+        type=$1
+        msg=$2
+        time=$(date '+%d/%m/%Y %H:%M:%S')
+        color=$BLUE
+
+        if [  $type = "ok"  ]; then
+            color=$GREEN
+        else 
+            if [  $type = "error"  ]; then
+                color=$RED
+            fi
+        fi
+
+        echo -e "$time ${color}[$type]${NC} $msg"
+        if [  $type = "error"  ]; then
+            exit 1
+        fi
+    fi
+}
+
 '''
     Formato y administración de discos.
     Esta función presupone el siguiente particionado de disco:
@@ -22,28 +50,38 @@ uefi=false
     !Función pendiente de automatizar.
 '''
 function adminDiscos {
+    log info "Iniciando administración de discos"
     if [  $uefi = true ]; then
         mkfs.vfat -F32 $boot
+        log ok "$boot BOOT formateado en modo UEFI "
     else
         mkfs.ext2 $boot
+        log ok "$boot BOOT formateado en modo BIOS "
     fi
 
     mkfs.ext4 $root
+    log ok "$root ROOT formateado"
     mkfs.ext4 $home
+    log ok "$home HOME formateado"
     mkswap $swap
     swapon $swap
+    log ok "$swap SWAP configurado"
     mount $root /mnt
+    log ok "$root ROOT montado en /mnt"
 
     if [  $uefi = true ]; then
         mkdir -p /mnt/boot/efi
         mount $boot /mnt/boot/efi
+        log ok "$boot BOOT montado en /mnt/boot/efi (UEFI)"
     else
         mkdir /mnt/boot
         mount $boot /mnt/boot
+        log ok "$boot BOOT montado en /mnt/boot (BIOS)"
     fi
 
     mkdir /mnt/home
     mount $home /mnt/home
+    log ok "$home HOME montado en /mnt/home"
 }
 
 '''
@@ -51,6 +89,7 @@ function adminDiscos {
     !Pendiente de extraer los paquetes para una mayor escala y abtracción.
 '''
 function instalacionBase {
+    log info "Inicialización e instalación de sistema base"
     pacstrap /mnt linux linux-firmware base base-devel networkmanager xf86-input-synaptics grub ntfs-3g gvfs xdg-user-dirs nano wpa_supplicant dialog
     if [  $uefi = true ]; then
         pacstrap /mnt efibootmgr
@@ -76,7 +115,7 @@ adminDiscos
 conexion=false
 while [  $conexion = false ]; do
     if ping -c1 google.com &> /dev/null; then
-	echo "Conexión correcta";
+	log ok "Conexión correcta";
 	conexion=true;
     else
 	wifi-menu;
