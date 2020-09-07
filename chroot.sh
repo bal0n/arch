a2bf092c22269892b27b3954ec00cc0b3ae9a2b9
fjba <fjba@pm.me>
2020-09-07 09:50:59 +0200

    Sustituida instalacion de aurman por yay en AUR config

--- chroot.sh
+++ chroot.sh
@@ -102,13 +102,20 @@ function configRed {
 
 function configAUR {
     cd /tmp
-    gpg --recv-key 465022E743D71E39
-    git clone https://aur.archlinux.org/aurman.git
-    cd aurman
+    
+    # gpg --recv-key 465022E743D71E39
+    # git clone https://aur.archlinux.org/aurman.git
+    # cd aurman
+    # makepkg -si
+    # sudo sed -i '/^#Color/s/^#//' /etc/pacman.conf
+    # aurman -Syu
+    # log ok "aurman instalado"
+
+    git clone https://aur.archlinux.org/yay.git
+    cd yay
     makepkg -si
-    sudo sed -i '/^#Color/s/^#//' /etc/pacman.conf
-    aurman -Syu
-    log ok "aurman instalado"
+    yay -Syu
+    log ok "yay instalado"
 }
 
 
